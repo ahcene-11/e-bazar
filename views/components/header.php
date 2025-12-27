03f4db5e1f73f9b01a2f3ff1666fa2b8836f0d84
Ahcene Amouchas <ahcene.amouchas@univ-rouen.fr>
2025-12-27 11:14:03 +0100

    icone profile

--- views/components/header.php
+++ views/components/header.php
@@ -18,7 +18,7 @@
                 <?php if (isset($_SESSION['user'])): ?>
                     <a href="index.php?action=dashboard">Mes annonces</a>
                     <a href="index.php?action=create" class="btn-primary-nav">Déposer une annonce</a>
-                    <a href="index.php?action=dashboard">👤 <?= htmlspecialchars($_SESSION['user']['email']) ?></a>
+                    <a href="index.php?action=dashboard"><img src="public/images/icone-utilisateur.png" alt="icone user" class="user-avatar"> <?= htmlspecialchars($_SESSION['user']['email']) ?></a>
                     
                     <?php if ($_SESSION['user']['role'] === 'admin'): ?>
                         <a href="index.php?action=admin" class="admin-link">⚙️ Admin</a>
